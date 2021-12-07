caaffd2fd7333634d9242eaa7f4703aac4965e3f
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-08 00:28:52 +0330

    use ViewChild and life cycle

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,18 +1,27 @@
-import {Component, ContentChild, ElementRef, OnInit} from '@angular/core';
+import {AfterContentChecked, AfterContentInit, Component, ContentChild, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, AfterContentInit, AfterContentChecked {
 
   @ContentChild('par', {static: true}) par!: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.par.nativeElement.textContent);
+    console.log("ngOnInit: " + this.par.nativeElement.textContent);
   }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked: " + this.par.nativeElement.textContent);
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit: " + this.par.nativeElement.textContent);
+  }
+
 
 }
