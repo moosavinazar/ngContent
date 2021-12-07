9be6cc0757aa0225e232ccd3671b20aa441279f2
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-08 00:21:28 +0330

    access ng-content information in child component

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ContentChild, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  @ContentChild('par', {static: true}) par!: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.par.nativeElement.textContent);
   }
 
 }
